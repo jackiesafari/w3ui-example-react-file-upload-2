cfcc20b8ad8507cd206bec0d9c4b0579d23396da
jackiesafari <107513244+jackiesafari@users.noreply.github.com>
2023-01-14 22:35:28 -0500

    Integrating Livepeer

--- src/App.js
+++ src/App.js
@@ -5,8 +5,30 @@ import { UploaderProvider } from '@w3ui/react-uploader'
 import ContentPage from './ContentPage'
 import logo from './logo.png'
 
+
+  import {
+  LivepeerConfig,
+  createReactClient,
+  studioProvider,
+} from '@livepeer/react';
+
+
+const client = createReactClient({
+ provider: studioProvider({ apiKey: '${process.env.LIVEPEER_API_KEY }'}),
+});
+
+function MyApp({ Component, pageProps }){
+
+  return (
+    <LivepeerConfig client={client}>
+     <Component {...pageProps} />
+    </LivepeerConfig>
+  );
+}
+     
 function App () {
   return (
+    
     <AuthProvider>
       <UploaderProvider>
         
@@ -15,6 +37,7 @@ function App () {
             <header>
               <img src={logo} width='250' alt='logo' />
             </header>
+            <h1 className = "text-6xl font-bold"> Stream from the Future </h1>
             <div className='w-90 w-50-ns mw6'>
               <ContentPage />
             </div>
@@ -23,6 +46,7 @@ function App () {
         
       </UploaderProvider>
     </AuthProvider>
+    
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import './tachyons.min.css'
 import App from './App'
 
+require('dotenv').config();
+
+console.log(process.env.LIVEPEER_API_KEY);
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
